b807863f547635de91e44c5be3e43cfd35b6bc8a
jesschan29 <jessicachan292003@gmail.com>
2024-01-16 10:48:34 +0800

    Fixed contactus form

--- pages/ContactUs/contactUsFormValidate.tsx
+++ pages/ContactUs/contactUsFormValidate.tsx
@@ -1,9 +1,9 @@
 const inputValidation = (fullName: string, user_message: string): boolean => {
-    if (fullName.trim() === "") {
+    if (fullName.length === 0 || /^\s+$/.test(fullName)) {
         alert("Please Enter Your Name!");
         return false;
     }
-    else if (user_message.trim() === "") {
+    else if (user_message.length === 0 || /^\s+$/.test(user_message)) {
         alert("Please Enter Your Queries!");
         return false;
     }
