936979e01dd450e95d1011212de504943523ef60
jesschan29 <jessicachan292003@gmail.com>
2024-01-16 12:32:24 +0800

    fixed stuff

--- pages/ContactUs/ContactUs.tsx
+++ pages/ContactUs/ContactUs.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
